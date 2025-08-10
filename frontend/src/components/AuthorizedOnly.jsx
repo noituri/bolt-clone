808a1b1c87dcdde532e416c790b5ac370c0a73b4
noituri <mikolaj.radkowski@protonmail.com>
2025-08-10 14:59:52 +0200

    Logout if token expired

--- frontend/src/components/AuthorizedOnly.jsx
+++ frontend/src/components/AuthorizedOnly.jsx
@@ -1,11 +1,19 @@
 import { Navigate } from "react-router";
 import { useAuth } from "../hooks/useAuth";
+import { useEffect } from "react";
+import { sendGetProfileRequest } from "../api/user";
 
 export default function AuthorizedOnly({ children }) {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+
+  useEffect(() => {
+    if (user) {
+      sendGetProfileRequest(user).catch(() => logout());
+    }
+  }, [user, logout]);
+
   if (!user) {
     return <Navigate to="/login" />;
   }
-
   return children;
 }
